a74364e59bc158b022311754057761b05c9a231b
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-08 20:43:12 +0300

    registering the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -27,6 +27,14 @@ def create_app(config_name):
     mail.init_app(app)
 
     
-    
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # setting config
+    # Will add the views and forms
+
 
     return app
