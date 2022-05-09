cec512efe10c56bcb689b9284fd0a5bae6a0c4ff
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-09 23:04:34 +0300

    create user table and pass in all the attributes

--- app/models.py
+++ app/models.py
@@ -19,3 +19,32 @@ class Role(db.Model):
     def __repr__(self):
         return f'User {self.name}'
 
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    comments = db.relationship('Comments',backref = 'comments',lazy = "dynamic")
+    pitches = db.relationship('Pitches',backref = 'author',lazy = "dynamic")
+    likes = db.relationship('Likes', backref = 'user', lazy = 'dynamic')
+    dislikes = db.relationship('Dislikes', backref = 'user', lazy = 'dynamic')
+    @property
+    def password(self):
+        raise AttributeError('You cannnot access this attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+    
