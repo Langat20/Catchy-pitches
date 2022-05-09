348ffcb3b8b2c91a80e36af8e97515e0c794bf46
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-09 23:01:48 +0300

    create role table and add it attributes

--- app/models.py
+++ app/models.py
@@ -8,3 +8,14 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'User {self.name}'
+
