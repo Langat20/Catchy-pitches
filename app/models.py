ddc181c5d50067cc3ccd3ecfb756deaa21b1125d
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-09 22:58:58 +0300

    add login manager

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from flask_login import UserMixin
+from . import db,login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
