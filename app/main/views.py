5f23784596dcaa263370c00c58dc95c5edd32ca6
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-09 12:13:25 +0300

    add a function to update the user profile

--- app/main/views.py
+++ app/main/views.py
@@ -65,3 +65,22 @@ def profile(uname):
 
     return render_template("profile/profile.html", user = user,pitch=pitch)
 
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
