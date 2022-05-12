22a336ce8c712a15576109971dfdb9062f51d720
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-12 13:18:28 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-from distutils.debug import DEBUG
 import os
 class Config:
     '''
@@ -6,7 +5,7 @@ class Config:
     '''
     SECRET_KEY =os.environ.get('SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL').replace("://","ql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://","ql://", 1)
 
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:langat20@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
@@ -18,53 +17,43 @@ class Config:
     SUBJECT_PREFIX='Pitches'
     SENDER_EMAIL='kipkuruie7.lang@gmail.com'
     # simple mde  configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
-
+    
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
     '''
     Production  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL').replace("://","ql://", 1)
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
-DEBUG=True
 
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:langat20@localhost/Pitches'
-    # SQLALCHEMY_DATABASE_URI=os.environ.get('ostgres://exagensvhukiqx:8ba9baf6e6b3074f0ab62a40356a5b96c9575e3f3609ebd733bf04c925311500@ec2-107-22-238-112.compute-1.amazonaws.com:5432/d92h52n2rrp8jo')
-    
-   
+DEBUG = True
+
 
 
 class TestConfig(Config):
-    
-   SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL').replace("://","ql://", 1)
-
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:langat20@localhost/pitches_test'
+    '''
+    Test
+    '''
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 
 class DevConfig(Config):
     '''
     Development  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL').replace("://","ql://", 1)
-
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:langat20@localhost/pitches'
-    
-    # DEBUG = True
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 
- 
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
-'test':TestConfig
-
+'test':TestConfig,
 }
